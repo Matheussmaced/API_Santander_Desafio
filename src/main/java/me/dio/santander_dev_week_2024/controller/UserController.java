544b103443a39dd1b96db804a3182516f555c0ad
Matheussmaced <mateusbyakuya3@gmail.com>
2024-06-28 12:03:40 -0300

    feat: creating method post

--- src/main/java/me/dio/santander_dev_week_2024/controller/UserController.java
+++ src/main/java/me/dio/santander_dev_week_2024/controller/UserController.java
@@ -1,10 +1,16 @@
 package me.dio.santander_dev_week_2024.controller;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 import me.dio.santander_dev_week_2024.domain.model.User;
 import me.dio.santander_dev_week_2024.service.UserService;
@@ -25,5 +31,13 @@ public class UserController {
   public ResponseEntity<User> findById(@PathVariable Long id) {
     var user = userService.findById(id);
     return ResponseEntity.ok(user);
+  }
+
+  @PostMapping
+  public ResponseEntity<User> create(@RequestBody User userToCreate) {
+    var userCreated = userService.create(userToCreate);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")// url of that id created in our user
+        .buildAndExpand(userCreated.getId()).toUri();
+    return ResponseEntity.created(location).body(userCreated);
   }
 }
