f5c418b740438193e597d611812676dde17a5b07
Matheussmaced <mateusbyakuya3@gmail.com>
2024-06-28 11:30:51 -0300

    feat: creating a service layer, registering an endpoint (User create) to register this information, inserting this data into our database and another endpoint to retrieve this information

--- src/main/java/me/dio/santander_dev_week_2024/domain/repository/UserRepository.java
+++ src/main/java/me/dio/santander_dev_week_2024/domain/repository/UserRepository.java
@@ -7,4 +7,6 @@ import me.dio.santander_dev_week_2024.domain.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  boolean existsByAccountNumber(String accountNumber);
 }
