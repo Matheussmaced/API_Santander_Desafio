65809e0c571418dcf4cf9f9b1a90eeb5d52e909a
Matheussmaced <mateusbyakuya3@gmail.com>
2024-06-29 17:06:49 -0300

    feat: handling post errors (inserting two identical users) and other errors

---
+++ src/main/java/me/dio/santander_dev_week_2024/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package me.dio.santander_dev_week_2024.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
+    return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+  }
+
+  @ExceptionHandler(Throwable.class)
+  public ResponseEntity<String> handleUnexpectedException(Throwable unexpectException) {
+    var message = "Unexpected server error, see the logs";
+
+    logger.error(message, unexpectException);
+    return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
